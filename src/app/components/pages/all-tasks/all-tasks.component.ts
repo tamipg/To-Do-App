7ae78b6ebf053e3b4fcd331a3320ed464bb5d655
tamipg <tami.pg94@gmail.com>
2024-01-27 07:00:48 +0100

    search

--- src/app/components/pages/all-tasks/all-tasks.component.ts
+++ src/app/components/pages/all-tasks/all-tasks.component.ts
@@ -3,20 +3,32 @@ import { FormsModule } from '@angular/forms';
 import { HttpService } from '../../../services/http.service';
 import { PageTitleComponent } from '../../page-title/page-title.component';
 import { TaskListComponent } from '../../task-list/task-list.component';
+import { StateService } from '../../../services/state.service';
+import { SearchHeaderComponent } from '../../search-header/search-header.component';
 
 @Component({
   selector: 'app-all-tasks',
   standalone: true,
-  imports: [FormsModule, PageTitleComponent, TaskListComponent],
+  imports: [FormsModule, PageTitleComponent, TaskListComponent, SearchHeaderComponent],
   templateUrl: './all-tasks.component.html',
   styleUrl: './all-tasks.component.scss'
 })
 export class AllTasksComponent {
   newTask="";
+  initialTaskList:any[]=[];
   taskList:any[]=[];
   httpService = inject(HttpService);
+  stateService = inject(StateService);
 
   ngOnInit(){
+    this.stateService.searchSubject.subscribe((value) => {
+      if(value){
+        this.taskList = this.initialTaskList.filter(x=>x.title.toLowerCase().includes(value.toLowerCase()));
+      }else{
+        this.taskList = this.initialTaskList;
+      }
+    });
+
     this.getAllTasks();
   }
 
@@ -29,7 +41,7 @@ export class AllTasksComponent {
 
   getAllTasks() {
     this.httpService.getAllTasks().subscribe((result:any)=>{
-      this.taskList=result;
+      this.initialTaskList = this.taskList = result;
     })
   }
 
@@ -50,7 +62,5 @@ export class AllTasksComponent {
       console.log("Important", task);
     })
   }
-
-  
 
 }
--- src/app/components/search-header/search-header.component.ts
+++ src/app/components/search-header/search-header.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Output, inject } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { StateService } from '../../services/state.service';
+import { debounceTime } from 'rxjs';
 @Component({
   selector: 'search-header',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './search-header.component.html',
   styleUrl: './search-header.component.scss'
 })
 export class SearchHeaderComponent {
+  stateService = inject(StateService);
+  searchText = new FormControl("");
 
+  ngOnInit(){
+    this.searchText.valueChanges.pipe(debounceTime(250)).subscribe((value) => {
+      this.stateService.searchSubject.next(value || '');
+    });
+  }
 }
