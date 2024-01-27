fc61aba2d14e67700c5959fa12cebb8bb7a0ae07
tamipg <tami.pg94@gmail.com>
2024-01-27 06:03:10 +0100

    completed tasks page

--- src/app/components/pages/completed-tasks/completed-tasks.component.ts
+++ src/app/components/pages/completed-tasks/completed-tasks.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpService } from '../../../services/http.service';
+import { PageTitleComponent } from '../../page-title/page-title.component';
+import { TaskListComponent } from '../../task-list/task-list.component';
 
 @Component({
   selector: 'app-completed-tasks',
   standalone: true,
-  imports: [],
+  imports: [PageTitleComponent,TaskListComponent],
   templateUrl: './completed-tasks.component.html',
   styleUrl: './completed-tasks.component.scss'
 })
 export class CompletedTasksComponent {
+  newTask="";
+  taskList:any[]=[];
+  httpService = inject(HttpService);
 
+  ngOnInit(){
+    this.getAllTasks();
+  }
+
+  getAllTasks() {
+    this.httpService.getAllTasks().subscribe((result:any)=>{
+      this.taskList=result.filter((x:any)=>x.completed===true);
+    })
+  }
+
+  onCompleted(task:any) {
+
+    task.completed = !task.completed;
+
+    this.httpService.UpdateTask(task).subscribe(()=>{
+      console.log("Completed", task);
+    })
+  }
+
+  onImportant(task:any) {
+    
+    task.important = !task.important;
+
+    this.httpService.UpdateTask(task).subscribe(()=>{
+      console.log("Important", task);
+    })
+  }
 }
